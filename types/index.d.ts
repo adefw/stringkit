444616c3b53775d0c9fcc986d27d5ef323802d0c
adefw <dev@adenil.com>
2020-04-18 23:42:40 +0800

    declare types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,9 @@
-export declare function toFullwidth(str: string): string
+export declare function toFullWidth(str: string): string
+
+export declare function toHalfWidth(str: string): string
+
+export declare function isFullWidth(str: string, global: boolean): boolean
+
+export declare function hasFullWidth(str: string): boolean
+
+export declare function ArabicToChinese(number: number, options?: { capital: false }): string
